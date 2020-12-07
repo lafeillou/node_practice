8226de9bcb210503bd46ba69135b9394bb020cef
lafeillou <lafeillou@126.com>
2020-12-07 19:13:54 +0800

    第六次作业

--- ex06/index.js
+++ ex06/index.js
@@ -4,15 +4,17 @@ module.exports.createToken = token => {
     if (ary.length !== 3) {
         return
     }
-
     return {
         getExp: () => {
+            let rawStr = Buffer.from(ary[1],'base64').toString('utf-8')
+            let payload = JSON.parse(rawStr)
+            return payload.exp
+        
             // ##BEGIN## 代码已加密
-gywgywgywgywgywgywgywgywgywgywgywgywgdqgdUgdvgdegdwgywgdYgdggRcgdPgdUgdggdcgywgcRgywgcwg9kg9cg9qgqDgdYgdggdmgdegd9gqRgdvgd9gRygywgcPgd=gddgddgd9gdmgqDgddgdmgdUgdDgqRgdggdmgRcg9YgqYg9mgqlgywgqdgdygdggdegd9gq=gqegqdgqkgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywgdYgdggRcgdPgdUgdggdcgqDgd9gRqgdY
+// gywgywgywgywgywgywgywgywgywgywgywgywgdqgdUgdvgdegdwgywgdYgdggRcgdPgdUgdggdcgywgcRgywgcwg9kg9cg9qgqDgdYgdggdmgdegd9gqRgdvgd9gRygywgcPgd=gddgddgd9gdmgqDgddgdmgdUgdDgqRgdggdmgRcg9YgqYg9mgqlgywgqdgdygdggdegd9gq=gqegqdgqkgqk
+// gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywgdYgdggRcgdPgdUgdggdcgqDgd9gRqgdY
             // ##END##
         },
-
         verify: key => {
             const hmac = crypto.createHmac('SHA256', key).update(ary[0]+ '.' +  ary[1]).digest('base64');
             return hmac === ary[2] + '='
